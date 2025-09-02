7b2baaf05cb13f9a5d974c4a74d6c63eeccb21ba
Venco <v3nco@v3nco.dev>
2025-09-02 21:42:54 +0200

    Some Structuring

--- Scripts/Guide/guide.gd
+++ Scripts/Guide/guide.gd
@@ -18,4 +18,4 @@ func _ready() -> void:
 	print("start waiting")
 	await wait(20)
 	print("finished waiting")
-	SceneManager.swap_scenes("res://Scenes/Ingame_Menu.tscn",get_tree().root,self,"fade_to_black")
+	SceneManager.swap_scenes("res://Scenes/Grid.tscn",get_tree().root,self,"fade_to_black")
--- Scripts/MainMenu/menu_root.gd
+++ Scripts/MainMenu/menu_root.gd
@@ -3,7 +3,7 @@ extends Control
 @onready var menu = $%VBoxContainer
 func _on_v_box_container_getout(guide) -> void:
 	if guide: SceneManager.swap_scenes("res://Scenes/Guide.tscn",get_tree().root,self,"fade_to_black")
-	else: SceneManager.swap_scenes("res://Scenes/Ingame_Menu.tscn",get_tree().root,self,"fade_to_black")
+	else: SceneManager.swap_scenes("res://Scenes/Grid.tscn",get_tree().root,self,"fade_to_black")
 func get_data():
 	var data = menu.difficulty
 	print("sent data: "+str(data))
